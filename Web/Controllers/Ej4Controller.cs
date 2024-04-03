5f5a6eb9a69b3b9361d714d2ecbdfdd12510bb81
MarianoGerosa <gerosamariano@gmail.com>
2024-04-03 13:37:03 -0300

    Ejercicio 4 resuelto

---
+++ Web/Controllers/Ej4Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public int Get([FromQuery] int var1, [FromQuery] int var2)
+        {
+            int mayor = var1 + var2;
+            return mayor;
+        }
+    }
+}
