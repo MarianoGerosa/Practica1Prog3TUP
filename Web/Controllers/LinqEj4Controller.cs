de2ce62579fffa0510abff6f31c76edfe9ab8e2b
MarianoGerosa <gerosamariano@gmail.com>
2024-04-10 15:03:47 -0300

    Ejercicio 4 de LinQ

---
+++ Web/Controllers/LinqEj4Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LinqEj4Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<int> Get([FromQuery] List<int> numList)
+        {
+            var query = (from int num in numList
+                         orderby num descending
+                         select num).ToList().GetRange(0, 5); 
+
+            return query;
+        }
+    }
+}
